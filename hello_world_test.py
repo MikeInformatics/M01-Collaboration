22bb5684ee36c67b7583e93c4c71ade3f4a15b56
MikeInformatics <144392845+MikeInformatics@users.noreply.github.com>
2023-10-29 13:38:51 -0400

    M01-Lab-1

--- hello_world_test.py
+++ hello_world_test.py
@@ -1,1 +1,8 @@
+# SDEV220-AVA-Fall-2023-Software Development-Python
+# Name      : M01 Lab Collaboration
+# Author    : M Mekoya
+# Date      : 10-29-2023
+# Version   : 1
+# Pourpose  : To push and commite file in to GitHub
 
+print ("Hello World!")
